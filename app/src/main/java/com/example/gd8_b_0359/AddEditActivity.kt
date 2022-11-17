b4aa3ae8f539e702ee174bdcdfba28a6a045f632
ASUS <jerrytang46@gmail.com>
2022-11-17 09:55:18 +0700

    Guided11

---
+++ app/src/main/java/com/example/gd8_b_0359/AddEditActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gd8_b_0359
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit)
+    }
+}
---
+++ app/src/main/java/com/example/gd8_b_0359/models/Mahasiswa.kt
@@ -0,0 +1,5 @@
+package com.example.gd8_b_0359.models
+
+class Mahasiswa (var nama: String, var npm: String, var fakultas: String, var prodi: String) {
+    var id: Long? =null
+}
